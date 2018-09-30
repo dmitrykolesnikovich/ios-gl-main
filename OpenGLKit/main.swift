18cc3c73db08f35d9ad8c0e06adc96edbbcfdafc
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2018-09-30 11:34:12 +0300

    refactoring

--- OpenGLKit/main.swift
+++ OpenGLKit/main.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Foundation
 
+// main!
 let argv = UnsafeMutableRawPointer(CommandLine.unsafeArgv).bindMemory(to: UnsafeMutablePointer<Int8>.self, capacity: Int(CommandLine.argc))
 UIApplicationMain(CommandLine.argc, argv, nil, NSStringFromClass(AppDelegate.self))
